f6a41f0d6c770a931e83d9aa20f23a13cc41ba7f
zeruFili <zerubabelfilmon1@gmail.com>
2026-09-25 17:36:23 +0300

    fixed the deployment path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'node:path'
 
 export default defineConfig(() => {
   return {
-    base: '/builders/',
+    base: '/kbn.marketplace/',
     plugins: [
       react(),
       tailwindcss(),
